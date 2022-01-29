6447d704f7ddad89e6095569757e0dfab2ed6221
Prateek <prateek_soni@outlook.com>
2022-01-29 23:17:50 +0530

    show posts in dashboard
    Signed-off-by: Prateek <prateek_soni@outlook.com>

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
 import { FC } from "react";
-import { useForm } from "react-hook-form";
-import Button from "../components/Button";
+import { FaThumbsDown, FaThumbsUp } from "react-icons/fa";
+import { useQuery } from "react-query";
 import CreatePost from "../components/CreatePost";
 import Header from "../components/Header";
 import PrivateHOC from "../hoc/PrivateHOC";
+import { privateApi } from "../services/api";
 
 const DashboardPage: FC = () => {
+  const {
+    data: posts,
+    isLoading,
+    refetch,
+  } = useQuery("fetchPosts", async () => {
+    const res = await privateApi.get("/api/v1/posts");
+    return res.data.posts;
+  });
+
+  const votePost = async (id: string, type: string) => {
+    await privateApi.get(`/api/v1/posts/vote/${id}/${type}`);
+    refetch();
+  };
+
   return (
     <PrivateHOC>
       <Head>
@@ -17,6 +32,38 @@ const DashboardPage: FC = () => {
         <div className="mt-12">
           <CreatePost />
         </div>
+        <section className="my-12">
+          {isLoading ? (
+            <h2>loading posts...</h2>
+          ) : (
+            posts.map((post: any) => (
+              <div key={post.id} className="p-8 shadow-md">
+                <p className="text-lg font-bold pb-2">@{post.user.username}</p>
+                <hr />
+                <div className="pt-2">
+                  <p className="text-3xl font-bold">{post.title}</p>
+                  <p className="text-xl leading-relaxed mt-2">{post.message}</p>
+                </div>
+                <div className="mt-8 flex gap-8">
+                  <div
+                    className="flex gap-3 items-center"
+                    onClick={() => votePost(post.id, "positive")}
+                  >
+                    <FaThumbsUp size={20} />
+                    {post.positiveCount}
+                  </div>
+                  <div
+                    className="flex gap-3 items-center"
+                    onClick={() => votePost(post.id, "negative")}
+                  >
+                    <FaThumbsDown size={20} />
+                    {post.negativeCount}
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
+        </section>
       </div>
     </PrivateHOC>
   );
